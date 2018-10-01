508420cab6e100d46a105fe19f2d99c15a9095af
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-01 22:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 function happyHolidays (){
   return "Happy holidays!";
 }
+happyHolidays()
 
 function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
+happyHolidayTo(holidays, you)
 
 function holidayCountdown(holiday,)
