d8e892b0ae8a209f684069fad3019ea9269a0e98
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-01 21:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays (){
+  return "Happy holidays!"
+}
+
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,)
